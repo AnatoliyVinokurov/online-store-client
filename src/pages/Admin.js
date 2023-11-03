8fc690f6d7698d62dd42ba0e4041ac500b19eb06
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-11-03 21:42:02 +0200

    added modals forms

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Container } from "react-bootstrap";
-import '../styles.css'; // Импортируем стили из файла styles.css
+import '../styles.css';
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateDevice from "../components/modals/CreateDevice";
+import CreateType from "../components/modals/CreateType"; // Импортируем стили из файла styles.css
 
 const Admin = () => {
+    const[brandVisible, setBrandVisible] = useState(false)
+    const[typeVisible, setTypeVisible] = useState(false)
+    const[deviceVisible, setDeviceVisible] = useState(false)
+
     return (
         <Container className="d-flex flex-column align-items-center admin-container">
-        <Button variant={"outline-dark"} className="mt-4 p-2 admin-button">Добавить тип</Button>
-            <Button variant={"outline-dark"} className="mt-4 p-2 admin-button">Добавить бренд</Button>
-            <Button variant={"outline-dark"} className="mt-4 p-2 admin-button">Добавить устройство</Button>
+        <Button variant={"outline-dark"} className="mt-4 p-2 admin-button" onClick={()=>setTypeVisible(true)}>Добавить тип</Button>
+            <Button variant={"outline-dark"} className="mt-4 p-2 admin-button" onClick={()=>setBrandVisible(true)}>Добавить бренд</Button>
+            <Button variant={"outline-dark"} className="mt-4 p-2 admin-button" onClick={()=>setDeviceVisible(true)}>Добавить устройство</Button>
+        <CreateBrand show={brandVisible} onHide={()=> setBrandVisible(false)}/>
+            <CreateDevice show={deviceVisible} onHide={()=> setDeviceVisible(false)}/>
+            <CreateType show={typeVisible} onHide={()=> setTypeVisible(false)}/>
         </Container>
     );
 };
